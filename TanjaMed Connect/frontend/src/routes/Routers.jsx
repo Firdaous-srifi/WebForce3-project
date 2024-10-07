744a3a550b6e9bf53cc973c05aa09037dbb00a3e
Firdaous <srififirdaous@gmail.com>
2024-10-07 03:33:10 +0100

    setup project

---
+++ TanjaMed Connect/frontend/src/routes/Routers.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorDetails from '../pages/Doctors/DoctorDetails'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Services from '../pages/Services'
+import Contact from '../pages/Contact'
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route>
+        <Home></Home>
+        <Home></Home>
+        <Doctors></Doctors>
+        <DoctorDetails></DoctorDetails>
+        <Login></Login>
+        <Signup></Signup>
+        <Contact></Contact>
+        <Services></Services>
+        </Route>
+    </Routes>
+  )
+}
+
+export default Routers

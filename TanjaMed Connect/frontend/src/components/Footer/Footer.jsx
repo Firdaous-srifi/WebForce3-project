1d6bed928a095e098e30c06976dd82087da279cf
Firdaous <srififirdaous@gmail.com>
2024-10-14 02:41:43 +0100

    FOOTER

--- TanjaMed Connect/frontend/src/components/Footer/Footer.jsx
+++ TanjaMed Connect/frontend/src/components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const socialLinks = {
 const quickLink01 = {
   {
     path:"/home",
-    display"Home",
+    display:"Home",
   },
   {
     path:"/",
@@ -45,7 +45,7 @@ const quickLink01 = {
 const quickLink02 = {
   {
     path:"/find-a-doctor",
-    display"Find a Doctor",
+    display:"Find a Doctor",
   },
   {
     path:"/",
@@ -74,9 +74,68 @@ const quickLink03 = {
 };
 
 const Footer = () => {
-  const year = new Date
+  const year = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <footer className='pb-16 pt-18'>
+      <div className='container'>
+        <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
+          <div>
+            <img src="TanjaMed Connect/frontend/src/assets/images/logo.png" alt="" />
+            <p className='text-[16px] leading-7 font-[400] text-text mt-4'>Copyright  {year} TanjaMed Connect. All rights reserved.</p>
+            <div className='flex items-center gap-3 mt-4'>
+              {socialLinks.map((link, index) => (
+                <Link to={link.path} key={index} className='w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primary hover:border-none'>
+                  {Link.icon}
+                </Link>
+              ))}
+            </div>
+          </div>
+      
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-heading'>
+               Quick Links
+            </h2>
+
+            <ul>
+              {quickLinks01.map((item, index) => (
+                <li key={index} className='mb-4'>
+                  <Link to={item.path} className='text-[16px] leading-7 font-[400] text-text'>{item.display}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-heading'>
+                I want to :
+            </h2>
+
+            <ul>
+              {quickLinks02.map((item, index) => (
+                <li key={index} className='mb-4'>
+                  <Link to={item.path} className='text-[16px] leading-7 font-[400] text-text'>{item.display}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-heading'>
+                Support
+            </h2>
+
+            <ul>
+              {quickLinks03.map((item, index) => (
+                <li key={index} className='mb-4'>
+                  <Link to={item.path} className='text-[16px] leading-7 font-[400] text-text'>{item.display}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 

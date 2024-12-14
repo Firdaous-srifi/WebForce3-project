5016c64f93ae7f5b8d0d2f2d16bd7e4327be8dc8
Firdaous <srififirdaous@gmail.com>
2024-12-14 14:40:26 +0100

    Update ServicesList.jsx

--- frontend/src/components/services/ServicesList.jsx
+++ frontend/src/components/services/ServicesList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import servicesCard from './ServicesCard'
-import {services} from "./../../data/services"
+import {services} from './../../data/services'
 
 const ServicesList = ({item, index}) => {
   return (
